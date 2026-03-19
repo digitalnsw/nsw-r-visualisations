9aa442f94d685beb8a2c6f5a43bf29ac076487cf
Carl Suster <carl.suster@sydney.edu.au>
2026-03-19 16:18:18 +1100

    Only register if font not installed

--- R/zzz.R
+++ R/zzz.R
@@ -1,26 +1,30 @@
 .onLoad <- function(libname, pkgname) {
-  plain <- system.file(
-    "fonts/Public_Sans/static/PublicSans-Regular.ttf",
-    package = pkgname
-  )
-  bold <- system.file(
-    "fonts/Public_Sans/static/PublicSans-Bold.ttf",
-    package = pkgname
-  )
-  italic <- system.file(
-    "fonts/Public_Sans/static/PublicSans-Italic.ttf",
-    package = pkgname
-  )
-  bolditalic <- system.file(
-    "fonts/Public_Sans/static/PublicSans-BoldItalic.ttf",
-    package = pkgname
-  )
+  has_public_sans <- "Public Sans" %in% systemfonts::system_fonts()$family
 
-  systemfonts::register_font(
-    "Public Sans",
-    plain = plain,
-    bold = bold,
-    italic = italic,
-    bolditalic = bolditalic
-  )
+  if (!has_public_sans) {
+    plain <- system.file(
+      "fonts/Public_Sans/static/PublicSans-Regular.ttf",
+      package = pkgname
+    )
+    bold <- system.file(
+      "fonts/Public_Sans/static/PublicSans-Bold.ttf",
+      package = pkgname
+    )
+    italic <- system.file(
+      "fonts/Public_Sans/static/PublicSans-Italic.ttf",
+      package = pkgname
+    )
+    bolditalic <- system.file(
+      "fonts/Public_Sans/static/PublicSans-BoldItalic.ttf",
+      package = pkgname
+    )
+
+    systemfonts::register_font(
+      "Public Sans",
+      plain = plain,
+      bold = bold,
+      italic = italic,
+      bolditalic = bolditalic
+    )
+  }
 }
